ce118ee4737066e32a57cb8bef3caee910c3eb04
zarnii <tryakin_gd1@mail.ru>
2026-06-19 15:41:30 +0300

    feat: JitterBuffer. Спроектировал  прототип интерфейса

---
+++ src/RTP/JitterBuffer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RTP
+{
+    /// <summary>
+    /// Джиттер-буфер. Принмает пакеты из RTP сессии и сглаживает их.
+    /// </summary>
+    internal class JitterBuffer
+    {
+        private readonly byte[] _buffer;
+
+        /// <summary>
+        /// Событие, сигнализирующее о заполнении буфера.
+        /// </summary>
+        public event Action<byte[]>? OnBufferFillng;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="buffer">Буфер, в который будут кладываться сэмлы.</param>
+        public JitterBuffer(byte[] buffer)
+        {
+            ArgumentNullException.ThrowIfNull(buffer, nameof(buffer));
+
+            _buffer = buffer;
+        }
+
+        public void ParseRawPacket(Span<byte> rawPacket)
+        {
+            if (rawPacket.Length < RtpHeader.FixedHeaderSize)
+            {
+                return; // ???
+            }
+
+
+        }
+    }
+}
